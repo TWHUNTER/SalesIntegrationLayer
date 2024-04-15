f88dc4e6e922847a5571ebfd5f79607c02455495
dwLuis Daniel De La Cruz García <luisdanieldlcg@gmail.com>
2024-04-15 17:05:27 -0400

    test integration database

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,20 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace IntegracionDesarrollo3.Models
 {
+    [Table("users")]
     public class UserModel
     {
         public int Id { get; set; }
+        [Column("full_name")]
         public string client_FullName { get; set; }
         public string Email { get; set; }
+
+        // its called phone_number in the database
+        [Column("phone_number")]
         public string PhoneNumber { get; set; }
-        public DateTime  CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
